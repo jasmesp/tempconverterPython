f9e52000403eaa24e14c5728a7fb0ec6672f63c0
Jas Palermo <jasmes@gmail.com>
2022-07-23 21:23:48 -0400

    added c to eff

--- tempconverter.py
+++ tempconverter.py
@@ -1,23 +1,34 @@
 
 def QuitOrConvert():
-    r = str(input("C to convert, q to quit"))
+    r = str(input("C to convert, q to quit\n"))
     if r.lower() == ("c"): #x.lower() converts str to lowercase
-        tempConverter()
+        Celsius_or_Fah()
     else: bye()
 
-def tempConverter():
-    f = float(input("degrees f"))
+def Celsius_or_Fah():
+    c_or_f = str(input("C for celsius to fahrenheit, F for fahrenheit to Celsius\n"))
+    if c_or_f.lower() == ("c"): #x.lower() converts str to lowercase
+        c_to_f_converter()
+    else: f_to_c_converter()
+
+def f_to_c_converter():
+    f = float(input("degrees f\n"))
     c = (f - 32.0)*(5/9)
     print(c)
     QuitOrConvert()
-    
-    
+
+
+def c_to_f_converter():
+    c = float(input("degrees c\n"))
+    f = ((c * (9/5)) + 32.0)
+    print(f)
+    QuitOrConvert()
+
+
 def bye():
     print("Bye!")
     SystemExit
-    
+
 
 QuitOrConvert()
 #tempConverter()
-
-
